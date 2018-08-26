0539bc8d15bdcd9b5f0173f1aae9c65f9eff7fc4
qinzhenxing <zhenxing.qin@leappmusic.com>
2018-08-26 18:23:30 +0800

    添加具体目录

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -22,4 +22,6 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:liuyun012/Js_frame.git master:gh-pages
 
+# 删除dist文件夹
 cd -
+rm -rf dist
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,67 @@
 module.exports = {
   base: '/Js_frame/',
-  title: '前端开发知识汇总-个人博客',
-  description: '个人在前端开发过程中整理积累的前端方面的知识',
+  dest: 'dist',
+  title: 'Liuyun Blog',
+  description: '个人在前端开发过程中整理的前端知识',
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+  ],
   themeConfig: {
     nav: [
       {text: '首页', link: '/'},
+      {text: '前端指南', link: '/guide/'},
+      {text: '全栈', link: '/full-stack/'},
+      {text: '其他', link: '/others/'},
       {text: 'xes-wxapp', link: 'https://liuyun012.github.io/xes-wxapp/' },
       {text: 'GitHub', link: 'https://github.com/liuyun012/Js_frame/'},
     ],
-    sidebar: [
-      {
-        title: '入门',
-        children: [
-          '/install/',
-          '/get-started/',
-        ]
-      },
-      {
-        title: '组件',
-        children: [
-          '/components/jade语法整理',
-        ]
-      },
-
-    ]
+    sidebar: {
+      '/guide/': guideSidebarConfig(),
+      '/full-stack/': fullSidebarConfig(),
+      '/others/': othersSidebarConfig(),
+    }
   }
 }
+
+// 前端指南左侧导航
+function guideSidebarConfig () {
+  return [
+    {
+      title: '前端指南',
+      collapsable: false,
+      children: [
+        '/get-started/',
+        '/html-source/',
+        '/Javascript-source/'
+      ]
+    }
+  ]
+}
+
+// 全栈左侧导航
+function fullSidebarConfig () {
+  return [
+    {
+      title: '全栈',
+      collapsable: false,
+      children: [
+        '/interview-source/',
+        '/utils/'
+      ]
+    }
+  ]
+}
+
+// 其他页面左侧导航
+function othersSidebarConfig () {
+  return [
+    {
+      title: '其他',
+      collapsable: false,
+      children: [
+        '/others/'
+      ]
+    }
+  ]
+}
+
