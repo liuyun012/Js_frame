86fdd35e949fd62a899784af278e7f1c16453746
qinzhenxing <zhenxing.qin@leappmusic.com>
2018-08-26 16:30:32 +0800

    目录结构重新梳理

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,12 @@
 module.exports = {
   base: '/Js_frame/',
-  title: '前端开发知识汇总',
+  title: '前端开发知识汇总-个人博客',
   description: '个人在前端开发过程中整理积累的前端方面的知识',
   themeConfig: {
     nav: [
-      {text: '主页', link: '/'},
-      {text: 'GitHub', link: 'https://github.com/liuyun012/Js_frame'},
+      {text: '首页', link: '/'},
+      {text: 'xes-wxapp', link: 'https://liuyun012.github.io/xes-wxapp/' },
+      {text: 'GitHub', link: 'https://github.com/liuyun012/Js_frame/'},
     ],
     sidebar: [
       {
