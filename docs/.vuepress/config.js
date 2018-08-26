1a2498461c175ff97e6ea0f489aaabd9c6905006
qinzhenxing <zhenxing.qin@leappmusic.com>
2018-08-26 11:28:54 +0800

    添加vuepress的配置文件

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  base: '/Js_frame/',
+  title: '前端开发知识汇总',
+  description: '个人在前端开发过程中整理积累的前端方面的知识',
+  themeConfig: {
+    nav: [
+      {text: '主页', link: '/'},
+      {text: 'GitHub', link: 'https://github.com/liuyun012/Js_frame'},
+    ],
+    sidebar: [
+      {
+        title: '入门',
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        children: [
+          '/components/button',
+          '/components/tabs',
+          '/components/input',
+          '/components/grid',
+          '/components/layout',
+          '/components/toast',
+          '/components/popover',
+        ]
+      },
+
+    ]
+  }
+}
